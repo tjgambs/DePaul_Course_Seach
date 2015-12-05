cd1c781ecb9c75d3dd0ac68b39773c5b3adfaf40
Tim <tjgambs@gmail.com>
2015-12-05 02:08:45 -0600

    Updated books

--- schools/depaul-university/py/make_amazon_links.py
+++ schools/depaul-university/py/make_amazon_links.py
@@ -1,8 +1,8 @@
 import json
 import isbnlib
 import glob
-import bottlenose
-from BeautifulSoup import BeautifulSoup
+import urllib
+from BeautifulSoup import BeautifulSoup as Soup
 
 def format_data():
 	file_names = glob.glob("../course_books/*.json")
@@ -19,7 +19,7 @@ def format_data():
 					try:
 						url ='http://www.amazon.com/gp/product/'+number+'/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN='+number+'&linkCode=as2&tag=mocksched-20&linkId=EMBDL7BV7IXRB44G'
 						url_for_checking = 'http://www.webproxy.net/view?q=http://www.amazon.com/gp/product/'+number+'/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN='+number+'&linkCode=as2&linkId=EMBDL7BV7IXRB44G'
-						if not check_url(number):
+						if not check_url(url_for_checking):
 							ret.append([d['status'][i],'',name])
 						else:
 							ret.append([d['status'][i],url,name])
@@ -29,13 +29,14 @@ def format_data():
 	with open('../books.json','w') as output:
 		json.dump(dic,output)
 
-def check_url(number):
-	amazon = bottlenose.Amazon('AKIAIJC6MAEHF3QC67NA', '2omychx9HcFOu3hc4+T1qT/BiykvSF9pyIEZmyUF', 'mocksched-20')
-	response = amazon.ItemLookup(ItemId=number, SearchIndex='Books', IdType='ISBN')
-	if 'is not a valid value for ItemId. Please change this value and retry your request' in response:
-		return False
-	else:
+def check_url(url):
+	html = urllib.urlopen(url).read()
+	soup = Soup(html)
+	td = soup.find('td',{'class':'sans','valign':'middle'})
+	if td == None:
 		return True
-
+	else:
+		return False
+	
 if __name__ == '__main__':
 	format_data()
