88d33afc4d3cb1bda1167af61d3d8774142f6ccd
Tim <tjgambs@gmail.com>
2015-12-04 23:07:34 -0600

    Updated code to use amazons api

--- schools/depaul-university/py/make_amazon_links.py
+++ schools/depaul-university/py/make_amazon_links.py
@@ -1,7 +1,8 @@
 import json
 import isbnlib
 import glob
-import requests
+import bottlenose
+from BeautifulSoup import BeautifulSoup
 
 def format_data():
 	file_names = glob.glob("../course_books/*.json")
@@ -18,7 +19,7 @@ def format_data():
 					try:
 						url ='http://www.amazon.com/gp/product/'+number+'/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN='+number+'&linkCode=as2&tag=mocksched-20&linkId=EMBDL7BV7IXRB44G'
 						url_for_checking = 'http://www.webproxy.net/view?q=http://www.amazon.com/gp/product/'+number+'/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN='+number+'&linkCode=as2&linkId=EMBDL7BV7IXRB44G'
-						if not check_url(url_for_checking):
+						if not check_url(number):
 							ret.append([d['status'][i],'',name])
 						else:
 							ret.append([d['status'][i],url,name])
@@ -28,15 +29,13 @@ def format_data():
 	with open('../books.json','w') as output:
 		json.dump(dic,output)
 
-def check_url(url):
-	try:
-		r = requests.head(url)
-		if r.status_code < 400:
-			return True
-		else:
-			return False
-	except requests.ConnectionError:
+def check_url(number):
+	amazon = bottlenose.Amazon('AKIAIJC6MAEHF3QC67NA', '2omychx9HcFOu3hc4+T1qT/BiykvSF9pyIEZmyUF', 'mocksched-20')
+	response = amazon.ItemLookup(ItemId=number, SearchIndex='Books', IdType='ISBN')
+	if 'is not a valid value for ItemId. Please change this value and retry your request' in response:
 		return False
+	else:
+		return True
 
 if __name__ == '__main__':
 	format_data()
