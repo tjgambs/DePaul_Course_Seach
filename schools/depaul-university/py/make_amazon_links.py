cc14e0d45a9dced440dcb90e1cb4567c609a4817
Tim <tjgambs@gmail.com>
2015-12-04 22:06:33 -0600

    Added isbn numbers to books

--- schools/depaul-university/py/make_amazon_links.py
+++ schools/depaul-university/py/make_amazon_links.py
@@ -14,7 +14,7 @@ def format_data():
 				ret = []
 				for i in range(len(d['isbns'])):
 					number = isbnlib.to_isbn10(d['isbns'][i])
-					name = ','.join(d['names'][i].split(',')[:-1])
+					name = ','.join(d['names'][i].split(',')[:-1]) + ', Isbn: ' + d['isbns'][i]
 					try:
 						url ='http://www.amazon.com/gp/product/'+number+'/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN='+number+'&linkCode=as2&tag=mocksched-20&linkId=EMBDL7BV7IXRB44G'
 						url_for_checking = 'http://www.webproxy.net/view?q=http://www.amazon.com/gp/product/'+number+'/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN='+number+'&linkCode=as2&linkId=EMBDL7BV7IXRB44G'
