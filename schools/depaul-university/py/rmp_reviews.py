a27114135dd6615638b679778b87b139abec5882
Tim <tjgambs@gmail.com>
2016-02-12 12:49:50 -0600

    Updated data

--- schools/depaul-university/py/rmp_reviews.py
+++ schools/depaul-university/py/rmp_reviews.py
@@ -5,26 +5,29 @@ def pages_of_reviews(id):
 	counter = 1
 	url = 'http://www.ratemyprofessors.com/paginate/professors/ratings?tid=' + str(id) + '&page=' + str(counter)
 	page = urllib.urlopen(url).read()
-	
+
 	data = json.loads(page)
+
 	number_of_reviews = int(data['remaining'])
 	return number_of_reviews/20+2
 
 def get_all_reviews(id):
-	reviews = []
-	for i in range(1,pages_of_reviews(id)+1):
-		url = 'http://www.ratemyprofessors.com/paginate/professors/ratings?tid=' + str(id) + '&page=' + str(i)
-		page=urllib.urlopen(url).read()
+	try:
+		reviews = []
+		for i in range(1,pages_of_reviews(id)+1):
+			url = 'http://www.ratemyprofessors.com/paginate/professors/ratings?tid=' + str(id) + '&page=' + str(i)
+			page=urllib.urlopen(url).read()
 
-		data = json.loads(page)
-		reviews.append(data['ratings'])
-	
-	return reviews
+			data = json.loads(page)
+			reviews.append(data['ratings'])
+		return reviews
+	except:
+		return 'dead'
 
 def format_reviews(id):
 	formatted_reviews = []
 	reviews = get_all_reviews(id)
-
+	if reviews == 'dead': return 'dead'
 	class_name = ''
 	grade_received = ''
 	date = ''
